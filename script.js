a72e285199450938d7c84531e7060979334e41f6
Mason <davismasont@gmail.com>
2022-10-03 17:50:33 -0700

    java is functional and quiz is complete with scorecard

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 //Arrays 
-var Question1 = ('1')
-var Question2 = ('2')
-var Question3 = ('3')
-var Question4 = ('4')
-var Question5 = ('5')
-var Question6 = ('6')
-var Question7 = ('7')
-var Question8 = ('8')
-var Question9 = ('9')
-var Question10 = ('10')
-var Answers1 = ['correct','incorrect','incorrect','incorrect']
-var Answers2 = ['correct','incorrect','incorrect','incorrect']
-var Answers3 = ['correct','incorrect','incorrect','incorrect']
-var Answers3 = ['correct','incorrect','incorrect','incorrect']
-var Answers4 = ['correct','incorrect','incorrect','incorrect']
-var Answers5 = ['correct','incorrect','incorrect','incorrect']
-var Answers6 = ['correct','incorrect','incorrect','incorrect']
-var Answers7 = ['correct','incorrect','incorrect','incorrect']
-var Answers8 = ['correct','incorrect','incorrect','incorrect']
-var Answers9 = ['correct','incorrect','incorrect','incorrect']
-var Answers10 = ['correct','incorrect','incorrect','incorrect']
+var Question1 = ('finish the word... Java-----')
+var Question2 = ('the peramiters for a for loop are?')
+var Question3 = ('Which bear is best?')
+var Question4 = ('what is the word for setting a variable')
+var Question5 = ('which symbol is used to symbolize an id')
+var Question6 = ('what is an array')
+var Question7 = ('what are the two types of loops')
+var Question8 = ('what language is used for styling webpages')
+var Question9 = ('what is the proper syntax for leaving comments in javaScript')
+var Question10 = ('in a time interval in javaScript, what is the time counted in?')
+var Answers1 = ['script','tea','core','time']
+var Answers2 = ['an initializing value & a peramiter to persist until','It has no peramiters','a variable & a function','a time interval, a function, and a speed in milliseconds']
+var Answers3 = ['False....Black bear','polor bear','koala bear','brown bear']
+var Answers4 = ['all of these are','let','const','var']
+var Answers5 = ['#','%','$','!']
+var Answers6 = ['a set of variables or values set up in an orrdered list','a single value connected to a word','the first child in an object','an html term for a list']
+var Answers7 = ['for and while','for and let','while and until','until and let']
+var Answers8 = ['CSS','HTML','JAvaScript','Python']
+var Answers9 = ['//','/!','/*','**/']
+var Answers10 = ['Milliseconds','seconds','cycles','months/10000']
 var q1 = [Question1,Answers1]
 var q2 = [Question2,Answers2]
 var q3 = [Question3,Answers3]
@@ -40,66 +39,133 @@ function randQues() {
     randArrEnd.push(randArrStart[randOrder])
     randArrStart.splice(randOrder, 1)
     }
+    
 }
+var answerSelect = document.querySelectorAll(".answerSelect")
 
 
+timeLeft = 0
 //Randomize andswers
-
+var correct = 0 
 function randAnsQuestOn() {
+  
     randAnsEnd = []
     randAnsStart = []
-    randAnsStart = randArrEnd[questOn][1]
+    for(i = 0;i<4;i++){
+        randAnsStart.push(randArrEnd[questOn+1][1][i])
+    }
     for( i=0 ; i < 4;i++){
     randAns = Math.floor(Math.random() * randAnsStart.length)
     randAnsEnd.push(randAnsStart[randAns])
     randAnsStart.splice(randAns, 1)
+
     }
 }
 
+function endGame(){
+    playName = prompt("What are your initials?")
+    playScore = Math.round(correct * (timeLeft/10))
+    scoreCard =  document.createElement("li")
+    scoreCard.textContent = playName + ":" + playScore
+    document.getElementById("scoreList").append(scoreCard)
+    document.getElementById("QAnswer").setAttribute('style', 'visibility: hidden;');
+    document.getElementById("questionHead").setAttribute('style', 'visibility: hidden;');
+    document.getElementById("start").setAttribute("style" ,"visibility: visible;");
+    document.getElementById("Score").setAttribute("style" ,"visibility: visible;");
+}
+function Highscore(){
+    clearInterval(timerInt)
+    document.getElementById("QAnswer").setAttribute('style', 'visibility: hidden;');
+    document.getElementById("questionHead").setAttribute('style', 'visibility: hidden;');
+    document.getElementById("start").setAttribute("style" ,"visibility: visible;");
+    document.getElementById("Score").setAttribute("style" ,"visibility: visible;");
+}
+
+
+
 var next = document.querySelector("#next")
 
-function nextQues(){
+function nextQues(event){
+    if(event.target.innerText === randArrEnd[questOn][1][0]){
+        correct++
+        document.getElementById(questOn-1).setAttribute("style","color: green;")
+    }
+    if(event.target.innerText != randArrEnd[questOn][1][0]){
+        timeLeft = timeLeft - 15
+        if(timeLeft < 0){
+            endGame()
+        }
+        document.getElementById("timer").innerHTML = "Time Left:"+timeLeft
+        document.getElementById(questOn-1).setAttribute("style","color: red;")
+    }
+    if (questOn < 9){
     askQues()
+    }
     questOn = questOn + 1
+    if (questOn > 9){
+        clearInterval(timerInt)
+        endGame()
+    }
 }
 
 function askQues(){
     randAnsQuestOn()
-    document.querySelector("#questionHead").innerHTML = randArrEnd[questOn][0]
+    document.querySelector("#questionHead").innerHTML = randArrEnd[questOn+1][0]
     document.querySelector("#ans1").innerHTML = randAnsEnd[0]
     document.querySelector("#ans2").innerHTML = randAnsEnd[1]
     document.querySelector("#ans3").innerHTML = randAnsEnd[2]
     document.querySelector("#ans4").innerHTML = randAnsEnd[3]
 }
-
+var questOn = 0
 
 
 function startQuiz(){
     questOn=0
     randQues()
     askQues()
-    console.log(document)
+    
     questOn = questOn + 1
-    document.getElementById("next").setAttribute("style" ,"visibility: visible;");
+    // document.getElementById("next").setAttribute("style" ,"visibility: visible;");
     document.getElementById("QAnswer").setAttribute('style', 'visibility: visible;');
     document.getElementById("questionHead").setAttribute('style', 'visibility: visible;');
     document.getElementById("start").setAttribute("style" ,"visibility: hidden;");
+    document.getElementById("Score").setAttribute("style" ,"visibility: hidden;");
+    timeLeft = 100
+    timerInt = setInterval(function(){
+        timeLeft--
+        document.getElementById("timer").innerHTML = "Time Left:"+timeLeft
+    },1000)
+    for(i=0; i<9;i++){
+        document.getElementById(i).setAttribute("style","color: black;")
+    }
 }
+
+
 
 
 // var questionNextDisplay = document.getElementById("#next")
 // console.log(questionNextDisplay.style)
-
+// for (i = 0; i < answerSelect.length; i++){
+//     answerSelect[i].addEventListener("click", checkAnswer)
+//     }
 
 // when start is pressed
 var startQ = document.querySelector("#start");
 startQ.addEventListener("click", startQuiz);
-next.addEventListener("click", nextQues)
-//compare choice to correct
+// next.addEventListener("click", nextQues)
+var scoreBtn = document.querySelector("#highscore")
+scoreBtn.addEventListener("click", Highscore)
+
+for (i = 0; i < answerSelect.length; i++){
+answerSelect[i].addEventListener("click", nextQues)
+}
+
+
+
 
 //dispaly score
 
-//display time
+
 
 // view highscore
 
